c15a67ca62ebc99dcb5e6761bd3322f1ad650f2d
Myan Indugula <indugumw@dukes.jmu.edu>
2025-03-25 17:34:47 -0400

    "Implementation through part 9"

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -33,6 +33,7 @@ urlpatterns += [
 from django.views.generic import RedirectView
 urlpatterns += [
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 # Use static() to add URL mapping to serve static files during development (only)
